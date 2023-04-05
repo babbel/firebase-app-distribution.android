aae5b15fda543a764c35daf373035aca9cd62004
IvanHalitskyi <107841595+IvanHalitskyi@users.noreply.github.com>
2023-04-05 11:51:03 +0300

    Changed file writing for service account content

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [ -n "${INPUT_SERVICECREDENTIALSFILE}" ] ; then
 fi
 
 if [ -n "${INPUT_SERVICECREDENTIALSFILECONTENT}" ] ; then
-    cat <<< "${INPUT_SERVICECREDENTIALSFILECONTENT}" > service_credentials_content.json
+    printf "${INPUT_SERVICECREDENTIALSFILECONTENT}" > service_credentials_content.json
     export GOOGLE_APPLICATION_CREDENTIALS="service_credentials_content.json"
 fi
 
